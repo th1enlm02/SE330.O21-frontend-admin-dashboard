b3a060c1175f6af642c6ab537875b799d3621bd1
th1enlm02 <21521460@gm.uit.edu.vn>
2024-04-29 16:38:03 +0700

    style: create Sidebar ui

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,24 @@
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+import React, {ReactNode} from 'react'
+
+const RootLayout = ({ children } : { children: ReactNode }) => {
+  return (
+    <main className='relative'>
+      <Navbar />
+      
+      <div className='flex'>
+        <Sidebar />
+
+        <section className='flex min-h-screen flex-1 flex-col px-6 py-6 pt-28
+        max-md:pd-14 sm:px-14'>
+            <div className='w-full'>
+                {children}
+            </div>
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default RootLayout
